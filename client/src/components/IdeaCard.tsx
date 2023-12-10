e23068d4846308ba9e87823c5a3ec1f7c5a60662
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-09 19:16:51 -0800

    redesigns the animation of the card

--- client/src/components/IdeaCard.tsx
+++ client/src/components/IdeaCard.tsx
@@ -13,6 +13,7 @@ const IdeaCard = (props: IdeaCardProps) => {
   return (
     <motion.div
       key={props.index}
+      className="bg-white mt-2 shadow-lg px-3 py-4 flex justify-start items-center border-white border-2"
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
@@ -21,34 +22,33 @@ const IdeaCard = (props: IdeaCardProps) => {
         ease: [0.4, 0, 0.2, 1],
       }}
       whileHover={{
-        scale: 1.05,
-        transition: { duration: 0.2, ease: "easeOut" },
+        scale: 1.03,
+        border: "2px solid #F2BA52",
+        transition: { duration: 0.2, ease: "easeIn" },
       }}
     >
-      <div className="bg-white mt-2 shadow-lg px-3 py-4 flex justify-start items-center transition-all ease-in">
-        <div className="bg-[#F2BA52] rounded-full w-12 h-12 mx-4 flex justify-center items-center">
-          <p className="font-semibold">{getInitials(props.idea.author)}</p>
-        </div>
+      <div className="bg-[#F2BA52] rounded-full w-12 h-12 mx-4 flex justify-center items-center">
+        <p className="font-semibold">{getInitials(props.idea.author)}</p>
+      </div>
 
-        <div className="flex flex-col flex-1 mx-4">
-          <p className="text-lg text-slate-600 font-semibold">
-            {props.idea.title}
+      <div className="flex flex-col flex-1 mx-4">
+        <p className="text-lg text-slate-600 font-semibold">
+          {props.idea.title}
+        </p>
+        <div className="flex justify-between text-slate-500 text-xs mt-1">
+          <p>
+            Created: {formatDateToCustomString(props.idea.date.toUTCString())}
           </p>
-          <div className="flex justify-between text-slate-500 text-xs mt-1">
-            <p>
-              Created: {formatDateToCustomString(props.idea.date.toUTCString())}
-            </p>
-            <p>
-              Edited: {formatDateToCustomString(props.idea.date.toUTCString())}
-            </p>
-          </div>
+          <p>
+            Edited: {formatDateToCustomString(props.idea.date.toUTCString())}
+          </p>
         </div>
+      </div>
 
-        <div className="flex mx-4">
-          <img src={upvote} className="w-6" />
-          <p className="mx-1 w-4 text-center">{props.idea.upvotes}</p>
-          <img src={upvote} className="w-6 rotate-180" />
-        </div>
+      <div className="flex mx-4">
+        <img src={upvote} className="w-6" />
+        <p className="mx-1 w-4 text-center">{props.idea.upvotes}</p>
+        <img src={upvote} className="w-6 rotate-180" />
       </div>
     </motion.div>
   );
