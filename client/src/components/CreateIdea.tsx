a4560798b44d3fa611d7f855cfba87805cd45ec6
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-12 18:18:32 -0800

    makes the divs rounded

--- client/src/components/CreateIdea.tsx
+++ client/src/components/CreateIdea.tsx
@@ -10,7 +10,7 @@ const CreateIdea = (props: CreateIdeaProps) => {
 
     return (
         <>
-            <div className="flex justify-center items-center px-3 py-4 mb-8 bg-white shadow-md">
+            <div className="flex justify-center items-center px-3 py-4 mb-8 bg-white shadow-md rounded-md">
                 <div className="flex justify-center items-center rounded-full h-12 w-12 bg-[#F2BA52] mx-4">
                     <p className="text-3xl font-semibold text-white">!</p>
                 </div>
@@ -18,7 +18,7 @@ const CreateIdea = (props: CreateIdeaProps) => {
                 <motion.input
                     type="text"
                     placeholder="Note an Idea"
-                    className="flex-1 py-4 pl-4 mr-4 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7]"
+                    className="flex-1 py-4 pl-4 mr-4 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md"
                     autoFocus={false}
                     whileHover={{ borderColor: "#F2BA52" }}
                     transition={{ duration: 0.3 }}
