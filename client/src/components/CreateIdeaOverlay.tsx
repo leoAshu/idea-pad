079e5deb345ead3690d53de3276852d6b213d0e1
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-13 20:33:56 -0800

    wraps the input elements in a form element and creates a dummy handleSubmit function

--- client/src/components/CreateIdeaOverlay.tsx
+++ client/src/components/CreateIdeaOverlay.tsx
@@ -1,10 +1,17 @@
 import { motion } from "framer-motion"
+import { FormEvent } from "react"
 
 interface CreateIdeaOverlay {
     toggleOverlay: () => void
 }
 
 const CreateIdeaOverlay = (props: CreateIdeaOverlay) => {
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+
+        props.toggleOverlay()
+    }
+
     return (
         <motion.div
             role="background-overlay"
@@ -13,15 +20,17 @@ const CreateIdeaOverlay = (props: CreateIdeaOverlay) => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
         >
-            <motion.div
+            <motion.form
                 role="popup-container"
                 className="bg-white shadow-md p-4 rounded-md flex flex-1 flex-col max-w-3xl"
                 onClick={(e) => e.stopPropagation()}
+                onSubmit={handleSubmit}
                 initial={{ opacity: 0, scale: 0.5, y: 50 }}
                 animate={{ opacity: 1, scale: 1, y: 0 }}
             >
                 <motion.input
                     type="text"
+                    required
                     placeholder="Title"
                     className="flex-1 py-4 px-3 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md"
                     whileHover={{ borderColor: "#F2BA52" }}
@@ -30,18 +39,21 @@ const CreateIdeaOverlay = (props: CreateIdeaOverlay) => {
                 />
 
                 <motion.textarea
-                    placeholder="Description (Optional)"
                     rows={10}
+                    placeholder="Description (Optional)"
                     className="py-4 px-3 mt-4 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md resize-none"
                     whileHover={{ borderColor: "#F2BA52" }}
                     whileFocus={{ borderColor: "#F2BA52" }}
                     transition={{ duration: 0.3 }}
                 />
 
-                <button className="py-4 w-1/3 mt-4 bg-[#F2BA52] text-white font-semibold rounded-md bg-opacity-75 hover:bg-opacity-100 transition-all duration-300">
+                <button
+                    type="submit"
+                    className="py-4 w-1/3 mt-4 bg-[#F2BA52] text-white font-semibold rounded-md bg-opacity-75 hover:bg-opacity-100 transition-all duration-300"
+                >
                     Add
                 </button>
-            </motion.div>
+            </motion.form>
         </motion.div>
     )
 }
