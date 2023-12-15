74a9e4ac25eb632e72e2439125d305b70063c6ec
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-15 11:56:05 -0800

    fixes bug: upvote button appears above header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header className="w-full flex justify-center items-center bg-white sm:px-8 px-4 py-5 shadow-md sticky top-0">
+        <header className="w-full flex justify-center items-center bg-white sm:px-8 px-4 py-5 shadow-md sticky top-0 z-10">
             <div className="text-4xl font-bold">
                 <span className="text-[#F2BA52]">!dea</span>Pad
             </div>
