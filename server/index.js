023808a3e04afe578cfed72c61ce30a9f8e3085f
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-16 16:20:51 -0800

    defines post route for creating new idea

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const { v4: uuidv4 } = require("uuid")
 
 const app = express()
 const PORT = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 let ideas = []
 
 app.get("/api/ideas", (req, res) => {
     res.status(200).json({ ideas })
+})
+
+app.post("/api/ideas", (req, res) => {
+    const { title, description } = req.body
+    const newIdea = {
+        id: uuidv4(),
+        title,
+        description,
+    }
+    res.status(200).json({ idea: newIdea })
 })
 
 app.listen(PORT, () => {
