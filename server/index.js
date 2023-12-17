6ea386c981da7c99ef1176c4d17b19fe9a8e93be
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-16 17:14:25 -0800

    defines a constant list of ideas and serves them over api

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 const { v4: uuidv4 } = require("uuid")
+const { ideas: dummyIdeas } = require("./ideas")
 
 const app = express()
 const PORT = 3000
 
+let ideas = dummyIdeas
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-let ideas = []
+app.get("/api", (req, res) => {
+    res.status(200).json({ message: "Server is up!" })
+})
 
 app.get("/api/ideas", (req, res) => {
     res.status(200).json({ ideas })
@@ -15,11 +22,18 @@ app.get("/api/ideas", (req, res) => {
 
 app.post("/api/ideas", (req, res) => {
     const { title, description } = req.body
+    if (!title) {
+        res.status(422).json({ error: "Title cannot be empty." })
+    }
+
     const newIdea = {
         id: uuidv4(),
+        date: new Date().toUTCString(),
         title,
         description,
+        upvotes: 1,
     }
+    ideas.push(newIdea)
     res.status(200).json({ idea: newIdea })
 })
 
