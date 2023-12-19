3dc87e9039c1abaf66ff244cf169ad6d876da1cb
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-18 18:16:11 -0800

    adds login and account name button depending on local state variable

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,33 @@
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+
 const Header = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true)
     return (
-        <header className="w-full flex justify-center items-center bg-white sm:px-8 px-4 py-5 shadow-md sticky top-0 z-10 select-none">
-            <div className="text-4xl font-bold">
+        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 shadow-md sticky top-0 z-10 select-none">
+            <div className="py-5 text-4xl font-bold">
                 <span className="text-[#F2BA52]">!dea</span>Pad
             </div>
+
+            {!isLoggedIn ? (
+                <motion.button
+                    className="px-6 py-2 text-lg font-semibold rounded-md"
+                    onClick={() => setIsLoggedIn(true)}
+                    animate={{ backgroundColor: 'rgba(242, 186, 82, 0.8)', color: 'white' }}
+                    whileHover={{ backgroundColor: 'rgba(242, 186, 82, 1)', color: 'white' }}
+                    whileTap={{ backgroundColor: 'rgba(242, 186, 82, 1)', color: 'white', scale: 0.95 }}
+                    transition={{ duration: 0.3 }}
+                >
+                    Login
+                </motion.button>
+            ) : (
+                <div
+                    className="bg-[#F2BA52] rounded-full w-14 h-14 flex justify-center items-center cursor-pointer"
+                    onClick={() => setIsLoggedIn(false)}
+                >
+                    <p className="font-semibold text-lg text-white">AO</p>
+                </div>
+            )}
         </header>
     )
 }
