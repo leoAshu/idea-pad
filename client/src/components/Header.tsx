64c5805c7cadbb13a7834596019195d6d2516e57
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-20 16:53:43 -0800

    adds max width to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,31 +5,33 @@ import useAuthContext from '../hooks/useAuthContextHook'
 const Header = () => {
     const { isLoggedIn, setIsLoggedIn } = useAuthContext()
     return (
-        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 shadow-md sticky top-0 z-10 select-none">
-            <div className="py-5 text-4xl font-bold">
-                <span className="text-[#F2BA52]">!dea</span>Pad
-            </div>
-
-            {!isLoggedIn ? (
-                <motion.button
-                    className="px-6 py-2 text-lg font-semibold rounded-md"
-                    onClick={() => setIsLoggedIn(true)}
-                    initial={{ backgroundColor: 'rgba(242, 186, 82, 0.8)', color: 'white' }}
-                    animate={{ backgroundColor: 'rgba(242, 186, 82, 0.8)', color: 'white' }}
-                    whileHover={{ backgroundColor: 'rgba(242, 186, 82, 1)', color: 'white' }}
-                    whileTap={{ backgroundColor: 'rgba(242, 186, 82, 1)', color: 'white', scale: 0.95 }}
-                    transition={{ duration: 0.3 }}
-                >
-                    Login
-                </motion.button>
-            ) : (
-                <div
-                    className="bg-[#F2BA52] rounded-full w-14 h-14 flex justify-center items-center cursor-pointer"
-                    onClick={() => setIsLoggedIn(false)}
-                >
-                    <p className="font-semibold text-lg text-white">AO</p>
+        <header className="w-full bg-white shadow-md sticky top-0 z-10 select-none">
+            <div className="max-w-7xl flex justify-between items-center sm:px-8 px-4 mx-auto">
+                <div className="py-5 text-4xl font-bold">
+                    <span className="text-[#F2BA52]">!dea</span>Pad
                 </div>
-            )}
+
+                {!isLoggedIn ? (
+                    <motion.button
+                        className="px-6 py-2 text-lg font-semibold rounded-md"
+                        onClick={() => setIsLoggedIn(true)}
+                        initial={{ backgroundColor: 'rgba(242, 186, 82, 0.8)', color: 'white' }}
+                        animate={{ backgroundColor: 'rgba(242, 186, 82, 0.8)', color: 'white' }}
+                        whileHover={{ backgroundColor: 'rgba(242, 186, 82, 1)', color: 'white' }}
+                        whileTap={{ backgroundColor: 'rgba(242, 186, 82, 1)', color: 'white', scale: 0.95 }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        Login
+                    </motion.button>
+                ) : (
+                    <div
+                        className="bg-[#F2BA52] rounded-full w-14 h-14 flex justify-center items-center cursor-pointer"
+                        onClick={() => setIsLoggedIn(false)}
+                    >
+                        <p className="font-semibold text-lg text-white">AO</p>
+                    </div>
+                )}
+            </div>
         </header>
     )
 }
