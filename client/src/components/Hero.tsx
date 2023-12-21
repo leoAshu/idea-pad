89efec0d73eb0c2cc803fd05a4a91c2a8c1017b8
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-20 17:26:53 -0800

    adds padding and margin for small screens

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,11 +1,13 @@
 const Hero = () => {
     return (
-        <div role="hero" className="my-12">
-            <h1 className="font-bold text-5xl">
+        <div role="hero" className="my-8 md:my-12">
+            <h1 className="font-bold text-3xl md:text-5xl">
                 <span className="text-[#F2BA52]">!dea</span>Pad
             </h1>
 
-            <p className="mt-2 text-lg font-medium">Note down your ideas for future reference and expand upon them!</p>
+            <p className="mt-2 text-sm md:text-lg md:font-medium">
+                Note down your ideas for future reference and expand upon them!
+            </p>
         </div>
     )
 }
