2d0373dfb3995effc30af3b43ac0afefd74c7d81
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-20 17:56:18 -0800

    optimizes the overlay for mobile screen size

--- client/src/components/NewIdeaOverlay.tsx
+++ client/src/components/NewIdeaOverlay.tsx
@@ -46,14 +46,14 @@ const NewIdeaOverlay = (props: NewIdeaOverlay) => {
     return (
         <motion.div
             role="background-overlay"
-            className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-[25%] backdrop-blur-md z-10"
+            className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-[25%] backdrop-blur-md z-10 text-sm md:text-base"
             onClick={() => props.closeOverlay()}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
         >
             <motion.form
                 role="popup-container"
-                className="bg-white shadow-md p-4 rounded-md flex flex-1 flex-col max-w-3xl"
+                className="bg-white shadow-md p-2 md:p-4 mx-3 md:mx-0 rounded-md flex flex-1 flex-col max-w-sm md:max-w-3xl"
                 onClick={(e) => e.stopPropagation()}
                 onSubmit={handleSubmit}
                 initial={{ opacity: 0, scale: 0.5, y: 50 }}
@@ -66,19 +66,19 @@ const NewIdeaOverlay = (props: NewIdeaOverlay) => {
                     placeholder="Title"
                     value={formData.title}
                     onChange={handleChange}
-                    className="flex-1 py-4 px-3 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md"
+                    className="flex-1 py-2 md:py-4 px-3 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md"
                     whileHover={{ borderColor: '#F2BA52' }}
                     whileFocus={{ borderColor: '#F2BA52' }}
                     transition={{ duration: 0.3 }}
                 />
 
                 <motion.textarea
-                    rows={10}
+                    rows={window.innerWidth >= 768 ? 12 : 8}
                     name="description"
                     placeholder="Description (Optional)"
                     value={formData.description}
                     onChange={handleChange}
-                    className="py-4 px-3 mt-4 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md resize-none"
+                    className="py-2 md:py-4 px-3 mt-2 md:mt-4 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md resize-none"
                     whileHover={{ borderColor: '#F2BA52' }}
                     whileFocus={{ borderColor: '#F2BA52' }}
                     transition={{ duration: 0.3 }}
@@ -86,7 +86,7 @@ const NewIdeaOverlay = (props: NewIdeaOverlay) => {
 
                 <motion.button
                     type="submit"
-                    className="py-4 w-1/3 mt-4 text-white font-semibold rounded-md"
+                    className="py-2 md:py-4 w-1/3 mt-2 md:mt-4 text-white font-semibold rounded-md"
                     animate={{ backgroundColor: '#F2BA52', opacity: 0.8 }}
                     whileHover={{ backgroundColor: '#F2BA52', opacity: 1 }}
                     whileTap={{ backgroundColor: '#F2BA52', scale: 0.95 }}
