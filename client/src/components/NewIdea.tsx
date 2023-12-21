3686ad53d2871c17503cf7099e8a9f4c319aca50
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-20 17:37:57 -0800

    fixes the input element to preventDefault on MouseDown

--- client/src/components/NewIdea.tsx
+++ client/src/components/NewIdea.tsx
@@ -12,21 +12,24 @@ const NewIdea = () => {
 
     return (
         <>
-            <div className="flex justify-center items-center px-3 py-4 mb-8 bg-white shadow-md rounded-md">
-                <div className="flex justify-center items-center rounded-full h-12 w-12 bg-[#F2BA52] mx-4">
-                    <p className="text-3xl font-semibold text-white">!</p>
+            <div className="flex justify-center items-center px-1 md:px-3 py-2 md:py-4 mb-8 bg-white shadow-md rounded-md">
+                <div className="flex justify-center items-center rounded-full h-8 w-8 md:h-12 md:w-12 bg-[#F2BA52] mx-2 md:mx-4">
+                    <p className="text-lg md:text-3xl font-black md:font-semibold text-white">!</p>
                 </div>
 
                 <motion.input
                     type="text"
                     placeholder="Note an Idea"
-                    className="flex-1 py-4 pl-4 mr-4 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md"
+                    className="flex-1 py-2 md:py-4 pl-2 md:pl-4 mr-2 md:mr-4 bg-[#F7F7F7] outline-none border-2 border-[#F7F7F7] rounded-md"
                     autoFocus={false}
                     whileHover={{ borderColor: '#F2BA52' }}
                     transition={{ duration: 0.3 }}
                     value={''}
                     onChange={() => {}}
-                    onClick={() => setIsOverlayActive(true)}
+                    onMouseDown={(e) => {
+                        e.preventDefault()
+                        setIsOverlayActive(true)
+                    }}
                 />
             </div>
 
