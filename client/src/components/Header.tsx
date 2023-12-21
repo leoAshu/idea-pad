96905bdc2c2b8e19be20c12e3e6dd57addf55550
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-20 18:01:14 -0800

    updates padding of header for mobile screen size

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,17 +4,17 @@ const Header = () => {
     const { isLoggedIn, setIsLoggedIn } = useAuthContext()
     return (
         <header className="w-full bg-white shadow-md sticky top-0 z-10 select-none">
-            <div className="max-w-7xl flex justify-between items-center sm:px-8 px-4 mx-auto">
-                <div className="py-5 text-4xl font-bold">
+            <div className="max-w-7xl flex justify-between items-center px-4 md:px-8 mx-auto">
+                <div className="py-3 md:py-5 text-2xl md:text-4xl font-bold">
                     <span className="text-[#F2BA52]">!dea</span>Pad
                 </div>
 
-                {isLoggedIn && (
+                {!isLoggedIn && (
                     <div
-                        className="bg-[#F2BA52] rounded-full w-14 h-14 flex justify-center items-center cursor-pointer"
+                        className="bg-[#F2BA52] rounded-full w-10 h-10 md:w-14 md:h-14 flex justify-center items-center cursor-pointer"
                         onClick={() => setIsLoggedIn(false)}
                     >
-                        <p className="font-semibold text-lg text-white">AO</p>
+                        <p className="font-semibold text-sm md:text-lg text-white">AO</p>
                     </div>
                 )}
             </div>
