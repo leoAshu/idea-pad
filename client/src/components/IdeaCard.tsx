2c682948231a4bfd9992c05f79a96e2bc9114b9d
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-20 18:21:21 -0800

    optimizes idea card for mobile screen size

--- client/src/components/IdeaCard.tsx
+++ client/src/components/IdeaCard.tsx
@@ -17,7 +17,7 @@ const IdeaCard = (props: IdeaCardProps) => {
     return (
         <motion.div
             key={index}
-            className="bg-white mt-2 shadow-md rounded-md px-3 py-4 flex justify-start items-center border-2 border-white"
+            className="bg-white mt-2 shadow-md rounded-md px-1 md:px-3 py-2 md:py-4 flex justify-start items-center border-2 border-white"
             initial={{ opacity: 0, scale: 0.8, y: 20 }}
             animate={{ opacity: 1, scale: 1, y: 0 }}
             exit={{ opacity: 0, scale: 0.8, y: -20 }}
@@ -31,37 +31,39 @@ const IdeaCard = (props: IdeaCardProps) => {
                 transition: { duration: 0.3, ease: 'easeIn' },
             }}
         >
-            <div className="bg-[#F2BA52] rounded-full w-12 h-12 mx-4 flex justify-center items-center">
-                <p className="font-semibold text-white">{getInitials(idea.author)}</p>
+            <div className="bg-[#F2BA52] rounded-full w-8 h-8 md:w-12 md:h-12 mx-2 md:mx-4 flex justify-center items-center">
+                <p className="text-white text-xs md:text-base font-black md:font-semibold">
+                    {getInitials(idea.author)}
+                </p>
             </div>
 
-            <div className="flex flex-col flex-1 mx-4">
-                <p className="text-lg text-slate-700 font-semibold">{idea.title}</p>
+            <div className="flex flex-col flex-1 mx-2 md:mx-4">
+                <p className="text-[0.9rem] md:text-lg text-slate-700 font-medium md:font-semibold">{idea.title}</p>
 
-                <p className="text-slate-500 text-xs mt-1">Created: {formatDateToCustomString(idea.date)}</p>
+                <p className="text-slate-500 text-xs mt-1 hidden md:block">
+                    Created: {formatDateToCustomString(idea.date)}
+                </p>
             </div>
 
-            <div className="flex mx-4 justify-center items-center">
+            <div className="flex mx-2 md:mx-4 justify-center items-center">
                 <motion.button
-                    className="px-0.5 py-1"
                     whileHover={{ scale: 1.15 }}
                     whileTap={{ scale: 0.9 }}
                     transition={{ duration: 0.2 }}
                     onClick={() => handleVote(idea.id, 1)}
                 >
-                    <img src={downvote} className="w-6 h-6 rotate-180" />
+                    <img src={downvote} className="w-4 md:w-6 h-4 md:h-6 rotate-180" />
                 </motion.button>
 
-                <p className="mx-1 w-4 text-slate-700 font-semibold text-right">{idea.upvotes}</p>
+                <p className="mx-1 w-4 text-slate-700 font-semibold text-right text-xs md:text-base">{idea.upvotes}</p>
 
                 <motion.button
-                    className="px-0.5 py-1"
                     whileHover={{ scale: 1.15 }}
                     whileTap={{ scale: 0.9 }}
                     transition={{ duration: 0.2 }}
                     onClick={() => handleVote(idea.id, -1)}
                 >
-                    <img src={downvote} className="w-6 h-6" />
+                    <img src={downvote} className="w-4 md:w-6 h-4 md:h-6" />
                 </motion.button>
             </div>
         </motion.div>
